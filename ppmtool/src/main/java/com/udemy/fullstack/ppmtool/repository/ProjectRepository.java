3cd63404b0fd64abdff9269100b3ff21a92c0ce1
sdmohite <sdmohite@gmail.com>
2018-12-05 08:44:03 +0530

    Project Object and Repository

---
+++ ppmtool/src/main/java/com/udemy/fullstack/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.udemy.fullstack.ppmtool.repository;
+
+import com.udemy.fullstack.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
