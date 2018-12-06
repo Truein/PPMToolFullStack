6fee2a45318fbd00d0d79d99bfdfb3748d8a6172
sdmohite <sdmohite@gmail.com>
2018-12-06 08:30:55 +0530

    Project Object Model Validations

--- ppmtool/src/main/java/com/udemy/fullstack/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/udemy/fullstack/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package com.udemy.fullstack.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -10,13 +14,26 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Project Name is required")
     private String projectName;
+
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min=4, max=6, message="Please use 4 to 6 Characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+
+    @NotBlank(message = "Project Description is required")
     private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     Project()
