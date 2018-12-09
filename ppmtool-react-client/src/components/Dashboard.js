7910ffb20ae586cc534c217dd55e5aef7eb5d6f1
sdmohite <sdmohite@gmail.com>
2018-12-09 19:36:32 +0530

    ReactJS Client App Added Header and Navbar

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+import Header from "./Layout/Header";
 
 class Dashboard extends Component {
   render() {
-    return <h1>Welcome to Dashboard</h1>;
+    return (
+      <div>
+        <h1>Welcome to Dashboard</h1>
+
+        <Header />
+        <ProjectItem />
+      </div>
+    );
   }
 }
 
