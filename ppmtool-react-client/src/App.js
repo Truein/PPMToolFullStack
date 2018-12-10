1d34b06c527a6642ff2e6fee0e6b9ad9a0d64d28
sdmohite <sdmohite@gmail.com>
2018-12-10 08:42:41 +0530

    PPMTool React Client app added route code for create button

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from "react";
 
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
+import Header from "./components/Layout/Header";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Dashboard />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          {
+            //not working properly
+          }
+          <Dashboard exact path="/dashboard" Component={Dashboard} />
+          <Route exact path="/addProject" Component={AddProject} />
+        </div>
+      </Router>
     );
   }
 }
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 import ProjectItem from "./Project/ProjectItem";
-import Header from "./Layout/Header";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import CreateProjectButton from "./Project/CreateProjectButton";
+
 class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1 className="alert alert-warning">Welcome to Dashboard</h1>
-
-        <Header />
-        <ProjectItem />
+        <div className="projects">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h1 className="display-4 text-center">Projects</h1>
+                <br />
+                <CreateProjectButton />
+                <br />
+                <hr />
+                <ProjectItem />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
